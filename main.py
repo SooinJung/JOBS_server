bc4b736e8dce8616ac57fe4e71009dc524e623c7
MoonRaindeer <ansdmsry04@sookmyung.ac.kr>
2024-10-17 20:01:06 +0900

    Trying to call openai api

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-import fastapi
+from fastapi import FastAPI
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
 
-app = fastapi.FastAPI()
+load_dotenv()
+
+app = FastAPI()
+client = OpenAI()
+client.api_key = os.getenv("OPENAI_API_KEY")
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+			"role": "system",
+			"content": "You are a helpful assistant. Answer in Korean."
+		},
+        {
+            "role": "user",
+            "content": "회계사 합격하는 법 알려줘."
+        }
+    ]
+)
+
+assistantAnswer = completion.choices[0].message.content
 
 @app.get('/')
 def home():
 	return {"message": "Welcome Home!"}
 
+@app.get('/q')
+def question():
+	return assistantAnswer
