dba0e4afad11b3e03c08ea453fe4a8f99f1d86e8
정수인 <92436387+SooinJung@users.noreply.github.com>
2025-02-10 19:14:24 +0900

    config update

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
 
+# 환경변수 정의
 HOST = "0.0.0.0"
 RHOST = HOST.replace(".", "\\.")
 PORT = 8080
@@ -9,3 +11,10 @@ ORIGIN_REGEX = f"^(https?://)?({RHOST}|localhost)(:({PORT}|{CPORT}|80|443))?(/.*
 CURR_DIR = os.getcwd()
 FILE_DIR = os.path.join(CURR_DIR, "files")
 MAX_FSIZE = 50 * 1024 * 1024 # 50MB
+
+# .env 파일 로드
+load_dotenv()
+
+# .env에서 가져온 api key
+API_KEY = os.getenv("api_key")
+PAFY_KEY = os.getenv("pafy_key")
